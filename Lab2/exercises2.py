3ec417b66601ce766c9e5c44afec2b9bd734e71f
DamirSmetov <144155872+DamirSmetov@users.noreply.github.com>
2024-01-26 13:23:09 +0600

    change the name

---
+++ Lab2/exercises2.py
@@ -0,0 +1,35 @@
+#Python Booleans
+#ex1
+print (10>9)
+#True
+#ex2
+print (10 == 9)
+#False
+#ex3
+print (10 < 9)
+#False
+#ex4
+print (bool("abc"))
+#True
+#ex5
+print (bool(0))
+#False
+#Python operators
+#ex1
+print (10 * 5)
+#ex2
+print (10 / 2)
+#ex3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+#ex4
+if 5 != 10:
+    print ("5 and 10 is not equal")
+#ex5
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
+#Python lists
+#ex1
+fruits = ["apple", "banana", "cherry"]
+print (fruits[1])
