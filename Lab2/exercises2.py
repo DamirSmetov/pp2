a6460fc2a08231db716e04ffc191b1a5df42911b
DamirSmetov <144155872+DamirSmetov@users.noreply.github.com>
2024-01-31 22:12:32 +0600

    Changes

--- Lab2/exercises2.py
+++ Lab2/exercises2.py
@@ -193,4 +193,23 @@ while i < 6:
     i += 1
 else:
     print("i is no longer less than 6")
-
+#Python For Loops
+#ex1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+#ex2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+#ex3
+for x in range(6):
+    print(x)
+#ex4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
