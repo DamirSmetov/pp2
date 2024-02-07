c7513f77642a5325a6ec51790d997e81d82dd466
DamirSmetov <144155872+DamirSmetov@users.noreply.github.com>
2024-02-08 01:22:55 +0600

    Create a class My_shape

--- Lab3/classes and objects.py
+++ Lab3/classes and objects.py
@@ -1,2 +1,11 @@
 class My_shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+    def __str__(self):
+        return f"The object is filled by {self.color}. This statement is {self.is_filled}"
+    def getArea(self):
+        return 0
     
+x = My_shape("green", True)
+print(x)
