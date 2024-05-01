9dea7734ce3b7552b7faa549fb85c7bdd62ab1cb
DamirSmetov <144155872+DamirSmetov@users.noreply.github.com>
2024-05-01 23:31:11 +0500

    Lab10

--- Lab10/Phonebook.py
+++ Lab10/Phonebook.py
@@ -3,18 +3,36 @@ import csv
 
 conn = psycopg2.connect(host = "localhost", dbname = "postgres", user="postgres", password = "damir2005d", port = "5432")
 conn.autocommit = True
-with conn.cursor() as cur:
-   cur.execute(
-        """CREATE TABLE Phonebook(
-            id serial PRIMARY KEY,
-            username VARCHAR(255),
-            number VARCHAR(20));
-             """)
-with conn.cursor() as cur:
-    cur.execute("INSERT INTO Phonebook (username, number) VALUES ('Damir', '+77083162100'), ('Ais', '+77017155141')")   
+#with conn.cursor() as cur:
+#   cur.execute(
+#        """CREATE TABLE Phonebook(
+#            id serial PRIMARY KEY,
+#            username VARCHAR(255),
+#            number VARCHAR(20));
+#             """)
+#with conn.cursor() as cur:
+#    cur.execute("INSERT INTO Phonebook (username, number) VALUES ('Damir', '+77083162100'), ('Ais', '+77017155141')")   
 
 
 #Entering username/number from console
+name = input("Enter username: ")
+number = input("Enter phonenumber: ")
+number = '+' + number
+with conn.cursor() as cur:
+    cur.execute(f"INSERT INTO Phonebook (username, number) VALUES ('{name}', '{number}')")
+    
+#Uploading data from CSV
+filename = "phones.csv"
+with open( filename, "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        s = row[0]
+        s = s.split()
+        with conn.cursor() as cur:
+            cur.execute(f"""INSERT INTO Phonebook (username, number) VALUES 
+                    ('{s[0]}', '{s[1]}');
+                    """)
+            
 
 #Updating data
 with conn.cursor() as cur:
@@ -29,4 +47,3 @@ with conn.cursor() as cur:
 #Deleting data
 with conn.cursor() as cur:
     cur.execute("DELETE FROM Phonebook WHERE username = 'Tima'")
-    
